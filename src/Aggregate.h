1ded932d3aad61209bfb383a7275837347d07e14
AlexLange1st <u24587312@tuks.co.za>
2025-10-25 22:36:54 +0200

    Iterator's implementation

--- src/Aggregate.h
+++ src/Aggregate.h
@@ -1,7 +1,14 @@
 #ifndef AGGREGATE_H
 #define AGGREGATE_H
 
+#include "Iterator.h"
+
+class Plant;
+
 class Aggregate {
+public:
+    virtual ~Aggregate() = default;
+    virtual Iterator<Plant>* createIterator(const std::string& season) = 0;
 };
 
 #endif
--- src/Iterator.h
+++ src/Iterator.h
@@ -1,7 +1,16 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
+class Plant;
+
+template<typename T>
 class Iterator {
+public:
+    virtual ~Iterator() = default;
+    virtual T* first() = 0;
+    virtual T* next() = 0;
+    virtual bool isDone() const = 0;
+    virtual T* currentItem() const = 0;
 };
 
 #endif
