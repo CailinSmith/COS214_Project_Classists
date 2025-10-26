70aa49fae97026f18b6e4647516769dd39683cac
J0rdanN89 <u24664155@tuks.co.za>
2025-10-26 15:25:34 +0200

    Extra unit testing to ensure plants are added to vectors in the inventoryManager correctly

--- testing/inventoryManagerTesting.cpp
+++ testing/inventoryManagerTesting.cpp
@@ -4,6 +4,20 @@
 #include "../src/Rose.h"
 #include "../src/Sunflower.h"
 #include "../src/Tomato.h"
+#include "../src/Basil.h"
+#include "../src/Lavender.h"
+#include "../src/Thyme.h"
+#include "../src/RubberTree.h"
+#include "../src/Ginger.h"
+#include "../src/Chamomile.h"
+#include "../src/AppleTree.h"
+#include "../src/OrangeTree.h"
+#include "../src/Lettuce.h"
+#include "../src/Pumpkin.h"
+#include "../src/Cattails.h"
+#include "../src/SnakePlant.h"
+#include "../src/Chrysanthemum.h"
+#include "../src/Pansy.h"
 
 TEST_CASE("InventoryManager - Constructor and Initial State") {
     InventoryManager* manager = new InventoryManager();
@@ -178,6 +192,184 @@ TEST_CASE("InventoryManager - Notification System") {
         CHECK_NOTHROW(manager->notifyStaff("Test message"));
         CHECK_NOTHROW(manager->notifyStaff(""));
         CHECK_NOTHROW(manager->notifyStaff("Plant added to inventory"));
+    }
+    
+    delete manager;
+}
+
+TEST_CASE("InventoryManager - Sorted Insertion by Category and Name") {
+    InventoryManager* manager = new InventoryManager();
+    
+    // Create plants from different categories
+    Rose rose;
+    Basil basil;
+    Lavender lavender;
+    Thyme thyme;
+    RubberTree rubberTree;
+    Ginger ginger;
+    Chamomile chamomile;
+    Tomato tomato;
+    AppleTree appleTree;
+    OrangeTree orangeTree;
+    Lettuce lettuce;
+    Pumpkin pumpkin;
+    Cattails cattails;
+    SnakePlant snakePlant;
+    Chrysanthemum chris;
+    Pansy pansy;
+    Sunflower sunflower;
+    
+    // Add plants in random order
+    manager->addToNursery(&rose);
+    manager->addToNursery(&basil);
+    manager->addToNursery(&lavender);
+    manager->addToNursery(&thyme);
+    manager->addToNursery(&rubberTree);
+    manager->addToNursery(&ginger);
+    manager->addToNursery(&chamomile);
+    manager->addToNursery(&tomato);
+    manager->addToNursery(&appleTree);
+    manager->addToNursery(&orangeTree);
+    manager->addToNursery(&lettuce);
+    manager->addToNursery(&pumpkin);
+    manager->addToNursery(&cattails);
+    manager->addToNursery(&snakePlant);
+    manager->addToNursery(&chris);
+    manager->addToNursery(&pansy);
+    manager->addToNursery(&sunflower);
+    
+    const vector<Plant*>& nurseryPlants = manager->getNurseryPlants();
+    
+    SUBCASE("Check total count") {
+        CHECK(nurseryPlants.size() == 17);
+    }
+    
+    SUBCASE("Check sorted by category priority") {
+        CHECK(nurseryPlants[0]->getCategory() == "Flower");
+        CHECK(nurseryPlants[1]->getCategory() == "Flower");
+        CHECK(nurseryPlants[2]->getCategory() == "Flower");
+        CHECK(nurseryPlants[3]->getCategory() == "Flower");
+        CHECK(nurseryPlants[4]->getCategory() == "Herb");
+        CHECK(nurseryPlants[5]->getCategory() == "Herb");
+        CHECK(nurseryPlants[6]->getCategory() == "Herb");
+        CHECK(nurseryPlants[7]->getCategory() == "Fruit");
+        CHECK(nurseryPlants[8]->getCategory() == "Fruit");
+        CHECK(nurseryPlants[9]->getCategory() == "Fruit");
+        CHECK(nurseryPlants[10]->getCategory() == "Vegetable");
+        CHECK(nurseryPlants[11]->getCategory() == "Vegetable");
+        CHECK(nurseryPlants[12]->getCategory() == "Aquatic");
+        CHECK(nurseryPlants[13]->getCategory() == "Indoor");
+        CHECK(nurseryPlants[14]->getCategory() == "Indoor");
+        CHECK(nurseryPlants[15]->getCategory() == "Medicinal");
+        CHECK(nurseryPlants[16]->getCategory() == "Medicinal");
+    }
+    
+    SUBCASE("Check alphabetical order within categories") {
+        CHECK(nurseryPlants[0]->getName() == "Chrysanthemum");
+        CHECK(nurseryPlants[1]->getName() == "Pansy");
+        CHECK(nurseryPlants[2]->getName() == "Rose");
+        CHECK(nurseryPlants[3]->getName() == "Sunflower");
+        CHECK(nurseryPlants[4]->getName() == "Basil");
+        CHECK(nurseryPlants[5]->getName() == "Lavender");
+        CHECK(nurseryPlants[6]->getName() == "Thyme");
+        CHECK(nurseryPlants[7]->getName() == "Apple Tree");
+        CHECK(nurseryPlants[8]->getName() == "Orange Tree");
+        CHECK(nurseryPlants[9]->getName() == "Tomato");
+        CHECK(nurseryPlants[10]->getName() == "Lettuce");
+        CHECK(nurseryPlants[11]->getName() == "Pumpkin");
+        CHECK(nurseryPlants[13]->getName() == "Rubber Tree");
+        CHECK(nurseryPlants[14]->getName() == "SnakePlant");
+        CHECK(nurseryPlants[15]->getName() == "Chamomile");
+        CHECK(nurseryPlants[16]->getName() == "Ginger");
+    }
+    
+    delete manager;
+}
+
+TEST_CASE("InventoryManager - Sorted Insertion to Sale by Category and Name") {
+    InventoryManager* manager = new InventoryManager();
+    
+    // Create plants from different categories
+    Rose rose;
+    Basil basil;
+    Lavender lavender;
+    Thyme thyme;
+    RubberTree rubberTree;
+    Ginger ginger;
+    Chamomile chamomile;
+    Tomato tomato;
+    AppleTree appleTree;
+    OrangeTree orangeTree;
+    Lettuce lettuce;
+    Pumpkin pumpkin;
+    Cattails cattails;
+    SnakePlant snakePlant;
+    Chrysanthemum chris;
+    Pansy pansy;
+    Sunflower sunflower;
+    
+    // Add plants to sale in random order
+    manager->addToSale(&rose);
+    manager->addToSale(&basil);
+    manager->addToSale(&lavender);
+    manager->addToSale(&thyme);
+    manager->addToSale(&rubberTree);
+    manager->addToSale(&ginger);
+    manager->addToSale(&chamomile);
+    manager->addToSale(&tomato);
+    manager->addToSale(&appleTree);
+    manager->addToSale(&orangeTree);
+    manager->addToSale(&lettuce);
+    manager->addToSale(&pumpkin);
+    manager->addToSale(&cattails);
+    manager->addToSale(&snakePlant);
+    manager->addToSale(&chris);
+    manager->addToSale(&pansy);
+    manager->addToSale(&sunflower);
+    
+    const vector<Plant*>& salePlants = manager->getForSalePlants();
+    
+    SUBCASE("Check total count") {
+        CHECK(salePlants.size() == 17);
+    }
+    
+    SUBCASE("Check sorted by category priority") {
+        CHECK(salePlants[0]->getCategory() == "Flower");
+        CHECK(salePlants[1]->getCategory() == "Flower");
+        CHECK(salePlants[2]->getCategory() == "Flower");
+        CHECK(salePlants[3]->getCategory() == "Flower");
+        CHECK(salePlants[4]->getCategory() == "Herb");
+        CHECK(salePlants[5]->getCategory() == "Herb");
+        CHECK(salePlants[6]->getCategory() == "Herb");
+        CHECK(salePlants[7]->getCategory() == "Fruit");
+        CHECK(salePlants[8]->getCategory() == "Fruit");
+        CHECK(salePlants[9]->getCategory() == "Fruit");
+        CHECK(salePlants[10]->getCategory() == "Vegetable");
+        CHECK(salePlants[11]->getCategory() == "Vegetable");
+        CHECK(salePlants[12]->getCategory() == "Aquatic");
+        CHECK(salePlants[13]->getCategory() == "Indoor");
+        CHECK(salePlants[14]->getCategory() == "Indoor");
+        CHECK(salePlants[15]->getCategory() == "Medicinal");
+        CHECK(salePlants[16]->getCategory() == "Medicinal");
+    }
+    
+    SUBCASE("Check alphabetical order within categories") {
+        CHECK(salePlants[0]->getName() == "Chrysanthemum");
+        CHECK(salePlants[1]->getName() == "Pansy");
+        CHECK(salePlants[2]->getName() == "Rose");
+        CHECK(salePlants[3]->getName() == "Sunflower");
+        CHECK(salePlants[4]->getName() == "Basil");
+        CHECK(salePlants[5]->getName() == "Lavender");
+        CHECK(salePlants[6]->getName() == "Thyme");
+        CHECK(salePlants[7]->getName() == "Apple Tree");
+        CHECK(salePlants[8]->getName() == "Orange Tree");
+        CHECK(salePlants[9]->getName() == "Tomato");
+        CHECK(salePlants[10]->getName() == "Lettuce");
+        CHECK(salePlants[11]->getName() == "Pumpkin");
+        CHECK(salePlants[13]->getName() == "Rubber Tree");
+        CHECK(salePlants[14]->getName() == "SnakePlant");
+        CHECK(salePlants[15]->getName() == "Chamomile");
+        CHECK(salePlants[16]->getName() == "Ginger");
     }
     
     delete manager;
